2b0d70d6d2a336c2933e7310e0a4fdfce2cc3009
ZotyDev <leonardokronbauer2002@gmail.com>
2021-01-20 00:04:29 -0300

    Fix bad repo

--- UnnamedEngine/src/application.cpp
+++ UnnamedEngine/src/application.cpp
@@ -1,6 +1,12 @@
 #include "Application.h"
 
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
 #include "time/timeManager.h"
 #include "logger/logger.h"
@@ -17,16 +23,32 @@ namespace UE
 
 	void Application::Run()
 	{
-		TimeManager::Update();
-		if (glfwInit())
+		// Initialize GLFW
+		if (!glfwInit())
 		{
-			LOG_FATAL("Not Working!");
+			LOG_FATAL("Failed to initialize GLFW!");
 		}
 
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+		GLFWwindow* window =glfwCreateWindow(800, 600, "Vulkan Window", nullptr, nullptr);
+
+		uint32_t extensionCount = 0;
+		vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+		LOG_INFO(extensionCount, " supported extensions!");
+
+		glm::mat4 matrix;
+		glm::vec4 vec;
+		auto test = matrix * vec;
 
 		Logger::ShowLog("latest.log");
-		while (true)
+
+		while (!glfwWindowShouldClose(window))
 		{
+			glfwPollEvents();
 		}
+
+		glfwDestroyWindow(window);
+		glfwTerminate();
 	}
 }
