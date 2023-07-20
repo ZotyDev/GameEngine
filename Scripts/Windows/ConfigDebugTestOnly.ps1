27cce50d3c0cedcadba02b8f298c8cf251eeffce
ZotyDev <leonardokronbauer2002@gmail.com>
2023-07-20 16:04:45 -0300

    Fixed 10

---
+++ Scripts/Windows/ConfigDebugTestOnly.ps1
@@ -0,0 +1,11 @@
+function Force-Resolve-Path {
+    param ([string] $FileName)
+    $FileName = Resolve-Path $FileName -ErrorAction SilentlyContinue -ErrorVariable _frperror
+    if (-not($FileName)) { $FileName = $_frperror[0].TargetObject }
+    return $FileName
+}
+
+$Project = Force-Resolve-Path $PSScriptRoot/../../CMakeLists.txt
+$Output  = Force-Resolve-Path $PSScriptRoot/../../build/Windows
+
+cmake -G "Ninja" -DCMAKE_TESTING=on -DCMAKE_BUILD_TYPE=Debug $Project -B $Output/Test
